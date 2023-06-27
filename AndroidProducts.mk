465218aa552c405f0549d89a5ec4548669483da5
Eidoron1 <eidoronp67@gmail.com>
2023-06-27 15:56:52 +0545

    alioth: Bringup for Derpfest

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/superior_alioth.mk
+    $(LOCAL_DIR)/derp_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    superior_alioth-user \
-    superior_alioth-userdebug \
-    superior_alioth-eng
+    derp_alioth-user \
+    derp_alioth-userdebug \
+    derp_alioth-eng
